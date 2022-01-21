681825e7b5d6bd9a29e411fb8fd59910fd8811fe
Houssame Rahiche <houssamerahiche@gmail.com>
2022-01-21 22:01:51 +0100

    The end of object section

--- Objects/Exercices/Blog.js
+++ Objects/Exercices/Blog.js
@@ -10,5 +10,17 @@ const psot = {
         author: '',
         body: ''
     }],
-    isLived: true
+    isLive: true
 }
+
+// Constructor function:
+function Post(title, body, author){
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = 0;
+    comments = [];
+    isLive = false;
+}
+
+const post1 = new Post('a','b','c')
